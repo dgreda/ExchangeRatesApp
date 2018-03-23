c1dd88093a48eb323bff94bb1a1f28ca5d333730
Damian Gręda <damian.greda@icloud.com>
2018-03-23 22:17:59 +0100

    Installed friendsofphp cs and removed the one from squizlabs. Adjusted travis build

--- src/Controller/ExchangeRatesController.php
+++ src/Controller/ExchangeRatesController.php
@@ -8,7 +8,6 @@ use App\Entity\ExchangeEnquiry;
 use App\ExchangeRates\ExchangeService;
 use App\Form\ExchangeEnquiryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
